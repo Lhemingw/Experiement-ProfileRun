f0e582e0712fb678f2fea888c90a3d63cf99d9b2
Lucinda Hemingway <136208461+Lhemingw@users.noreply.github.com>
2024-12-30 20:19:23 -0700

    Add files via upload
    Trying to put my web site together, need help.  Trying to put together old one I used for week/ add nav bar and put extra stuff in

---
+++ notes.js
@@ -0,0 +1,3 @@
+//open running website when doing this, I may need others that I have done to make this look good
+//https://www.codewithfaraz.com/content/438/create-simple-webpage-layout-using-html-and-css#google_vignette
+//need to find other stuff
